0c8c2369e94d2357f05bff5d671693f88fb91e95
swei <song.wei@anzhi-auto.cn>
2019-02-15 13:36:40 +0800

    first commit

---
+++ saveImage.py
@@ -0,0 +1,22 @@
+import os
+import urllib.request
+
+
+cur_path = os.getcwd()
+source_url_path = os.path.join(cur_path, 'raw_data')
+image_dst_path = os.path.join(cur_path, 'img_data')
+
+if not os.path.exists(image_dst_path):
+	os.makedirs(image_dst_path)
+
+for roots, parents, files in os.walk(source_url_path):
+	for f in files:
+		filename = os.path.join(roots, f)
+		prefix = filename.split('/')[-3] + '__' + filename.split('/')[-2] 
+		img_list_queue = open(filename, "r").readlines()
+		for img_list in img_list_queue:
+			image = img_list.split('\n')[0]
+			imagename = prefix + '__' + image.split('/')[-1]
+			abs_imagename = os.path.join(image_dst_path, imagename)
+			urllib.request.urlretrieve(image,filename=abs_imagename)
+
